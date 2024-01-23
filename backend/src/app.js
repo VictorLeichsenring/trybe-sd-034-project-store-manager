95aa6a12153582e24ec44c5122281c843784faec
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-01-23 00:18:12 -0300

    Apagado comentario de commit inicial

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const app = express();
 
-//comentario para iniciar o projeto
-
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.json({ status: 'Store Manager UP!' });
