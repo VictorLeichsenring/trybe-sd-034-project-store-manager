54d10a52898e24ce8c4e96e66cb184c7aaeefdf2
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-01-23 00:16:57 -0300

    Iniciando o projeto

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+//comentario para iniciar o projeto
+
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.json({ status: 'Store Manager UP!' });
